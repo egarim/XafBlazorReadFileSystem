9458665388563f8ddf82f31f37e8009eb2d209e2
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-03-22 09:32:17 +0300

    Create Directory

--- XafBlazorReadFileSystem.Blazor.Server/Controllers/FIlesViewController.cs
+++ XafBlazorReadFileSystem.Blazor.Server/Controllers/FIlesViewController.cs
@@ -21,7 +21,8 @@ namespace XafBlazorReadFileSystem.Blazor.Server.Controllers
     // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
     public partial class FilesViewController : ObjectViewController<ListView, FileSystemItem>
     {
-        SimpleAction CreateDirectory;
+        PopupWindowShowAction CreateDirectory;
+        SimpleAction CreateDirectory2;
         // Use CodeRush to create Controllers and Actions with a few keystrokes.
         // https://docs.devexpress.com/CodeRushForRoslyn/403133/
         public FilesViewController()
@@ -32,12 +33,58 @@ namespace XafBlazorReadFileSystem.Blazor.Server.Controllers
 
             //TODO use this https://www.syncfusion.com/blazor-components/blazor-file-manager
 
-            CreateDirectory = new SimpleAction(this, "CreateDirectory", "View");
+            //CreateDirectory2 = new SimpleAction(this, "CreateDirectory2", "View");
+            //CreateDirectory2.Execute += CreateDirectory_Execute;
+            //CreateDirectory2.TargetObjectsCriteria = CriteriaOperator.FromLambda<FileSystemItem>(f => f.Type == "directory").ToString();
+            //CreateDirectory2.TargetObjectsCriteriaMode = TargetObjectsCriteriaMode.TrueAtLeastForOne;
+            //CreateDirectory2.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+
+            CreateDirectory = new PopupWindowShowAction(this, "CreateDirectory", "View");
             CreateDirectory.Execute += CreateDirectory_Execute;
+            CreateDirectory.CustomizePopupWindowParams += CreateDirectory_CustomizePopupWindowParams;
             CreateDirectory.TargetObjectsCriteria = CriteriaOperator.FromLambda<FileSystemItem>(f => f.Type == "directory").ToString();
             CreateDirectory.TargetObjectsCriteriaMode = TargetObjectsCriteriaMode.TrueAtLeastForOne;
             CreateDirectory.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+
+
+
+
+
             // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        private void CreateDirectory_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            var selectedPopupWindowObjects = e.PopupWindowViewSelectedObjects[0] as DirectoryName;
+            var selectedSourceViewObjects = e.SelectedObjects[0];
+
+            var fsi = selectedSourceViewObjects as FileSystemItem;
+            if (fsi != null)
+            {
+                FileSystemHelper.CreateFolder(fsi, selectedPopupWindowObjects.Name);
+                var CurrentUsersDirectory = masterFrame.View.CurrentObject as UsersDirectory;
+                var NewFiles = FileSystemHelper.ReadFileSystem(CurrentUsersDirectory.Path);
+                foreach (var item in NewFiles)
+                {
+                    if (CurrentUsersDirectory.Files.FirstOrDefault(f => f.FullPath == item.FullPath) != null)
+                    {
+                        continue;
+                    }
+                    CurrentUsersDirectory.Files.Add(item);
+
+                }
+
+            }
+
+            // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112723/).
+        }
+        private void CreateDirectory_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            var Os=this.Application.CreateObjectSpace(typeof(DirectoryName));
+            var Params= Os.CreateObject<DirectoryName>();
+            e.View=this.Application.CreateDetailView(Os, Params);
+
+            // Set the e.View parameter to a newly created view (https://docs.devexpress.com/eXpressAppFramework/112723/).
         }
         private Frame masterFrame;
         public void AssignMasterFrame(Frame parentFrame)
@@ -48,23 +95,7 @@ namespace XafBlazorReadFileSystem.Blazor.Server.Controllers
         private void CreateDirectory_Execute(object sender, SimpleActionExecuteEventArgs e)
         {
 
-            var fsi=e.CurrentObject as FileSystemItem;
-            if (fsi != null)
-            {
-                FileSystemHelper.CreateFolder(fsi, "New Directory");
-                var CurrentUsersDirectory= masterFrame.View.CurrentObject as UsersDirectory;
-                var NewFiles=FileSystemHelper.ReadFileSystem(CurrentUsersDirectory.Path);
-                foreach (var item in NewFiles)
-                {
-                    if(CurrentUsersDirectory.Files.FirstOrDefault(f=>f.FullPath== item.FullPath)!=null)
-                    {
-                        continue;
-                    }
-                    CurrentUsersDirectory.Files.Add(item);
-
-                }
-             
-            }
+            
             // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112737/).
         }
         protected override void OnActivated()
---
+++ XafBlazorReadFileSystem.Module/BusinessObjects/DirectoryName.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafBlazorReadFileSystem.Module.BusinessObjects
+{
+    [DomainComponent]
+    public class DirectoryName : NonPersistentBaseObject
+    {
+        public string Name { get; set; }
+
+    }
+}
